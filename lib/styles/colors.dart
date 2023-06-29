7a7246d9deb6aab8c72dcfda8ebe6ae411bb2b7f
gustavool1 <go8895806@gmail.com>
2023-06-29 08:17:04 -0300

    feat: adding styles, colors and fonts

---
+++ lib/styles/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorPackage {
+  static const Color defaultPrimary = Color(0xffF3F6FC);
+  static const Color white = Colors.white;
+  static const Color blue = Color(0xff1270FF);
+  static const Color lightGray = Color(0xff9DADC6);
+  static const Color darkGray = Color(0xff3A4968);
+  static const Color black = Colors.black;
+}
---
+++ lib/styles/fonts.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:medicine_tracker/styles/colors.dart';
+
+class TextFonts {
+  static final _monteserratBaseStyle = GoogleFonts.openSans(
+    fontWeight: FontWeight.normal,
+    color: ColorPackage.black,
+  );
+
+  static final head2 = _monteserratBaseStyle.copyWith(fontSize: 18);
+
+  static final body1 = _monteserratBaseStyle.copyWith(fontSize: 16);
+}
