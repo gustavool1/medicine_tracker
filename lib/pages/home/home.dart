9b80cc455c11f5b900f51532ceb38586b9659bc0
gustavool1 <go8895806@gmail.com>
2023-08-25 10:17:21 -0300

    refact: refactoring home

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,33 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:medicine_tracker/pages/create_pill/create_pill.dart';
 import 'package:medicine_tracker/pages/medicines/medicines.dart';
 import 'package:medicine_tracker/styles/colors.dart';
+import 'package:medicine_tracker/ui/calendar/calendar.dart';
 import '../../bloc/bloc.dart';
 import '../../ui/ui.dart';
+import '../create_pill/create_pill.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  Widget getContent(int index) {
+  Widget getContent(int index, BuildContext context) {
     switch (index) {
       case 0:
-        return homeContent;
+        return homeContent(context);
 
       case 1:
         return const MedicinesPage();
 
       default:
-        return homeContent;
+        return homeContent(context);
     }
   }
 
-  Widget get homeContent => Container(
-        color: ColorPackage.defaultPrimary,
-        child: const Padding(
-          padding: EdgeInsets.only(top: 16.0),
-          child: PillsList(),
-        ),
+  Widget homeContent(BuildContext context) => Column(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              color: ColorPackage.white,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(45),
+                bottomRight: Radius.circular(45),
+              ),
+            ),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Icon(
+                      Icons.account_circle_rounded,
+                      color: ColorPackage.blue,
+                      size: 40,
+                    ),
+                    GestureDetector(
+                      onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (_) => const CreatePillPage())),
+                      child: const Icon(
+                        Icons.add_circle_rounded,
+                        color: ColorPackage.blue,
+                        size: 40,
+                      ),
+                    )
+                  ],
+                ),
+                const Calendar(),
+              ],
+            ),
+          ),
+          Container(
+            color: ColorPackage.defaultPrimary,
+            child: const Padding(
+              padding: EdgeInsets.only(top: 16.0),
+              child: PillsList(),
+            ),
+          ),
+        ],
       );
 
   @override
@@ -35,31 +75,7 @@ class HomePage extends StatelessWidget {
     return BlocBuilder<HomeBloc, HomeState>(
       builder: (context, state) {
         return Scaffold(
-          appBar: AppBar(
-            backgroundColor: ColorPackage.white,
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Icon(
-                  Icons.account_circle_rounded,
-                  color: ColorPackage.blue,
-                  size: 40,
-                ),
-                GestureDetector(
-                  onTap: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (_) => const CreatePillPage())),
-                  child: const Icon(
-                    Icons.add_circle_rounded,
-                    color: ColorPackage.blue,
-                    size: 40,
-                  ),
-                )
-              ],
-            ),
-          ),
-          body: getContent(state.index),
+          body: SafeArea(child: getContent(state.index, context)),
           bottomNavigationBar: const BottomNavigator(),
         );
       },
