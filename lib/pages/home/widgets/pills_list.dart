e7b007f250d476cb545b58243a87a1eacd25e22e
gustavool1 <go8895806@gmail.com>
2023-08-25 15:49:44 -0300

    feat: rendering depending on pill state

--- lib/pages/home/widgets/pills_list.dart
+++ lib/pages/home/widgets/pills_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:medicine_tracker/bloc/bloc.dart';
+import 'package:medicine_tracker/styles/styles.dart';
 import './pill.dart';
 
 class PillsList extends StatelessWidget {
@@ -14,9 +15,39 @@ class PillsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<PillsBloc, PillsState>(builder: (context, state) {
-      return Column(
-        children: state.pills.map((pill) => Pill(pill: pill)).toList(),
-      );
+      if (state is PillEmpty) {
+        return Center(
+          child: Text(
+            "Não há medicamentos para esse dia ",
+            style: TextFonts.head2.copyWith(
+              color: ColorPackage.darkBlue,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        );
+      }
+      if (state is PillInitial) {
+        context
+            .read<PillsBloc>()
+            .add(PillsEventGetPillsByDay(day: DateTime.now()));
+
+        return const Center(
+          child: CircularProgressIndicator(
+            color: ColorPackage.blue,
+          ),
+        );
+      }
+      if (state is PillLoading) {
+        return const Center(
+          child: CircularProgressIndicator(
+            color: ColorPackage.blue,
+          ),
+        );
+      } else {
+        return Column(
+          children: state.pills.map((pill) => Pill(pill: pill)).toList(),
+        );
+      }
     });
   }
 }
