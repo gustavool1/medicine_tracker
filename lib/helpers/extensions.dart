8ad26fd344a3c91fdb5d9dc9d4e0453e39eace97
gustavool1 <go8895806@gmail.com>
2023-08-25 15:48:22 -0300

    feat: adding new extensions to datetime

--- lib/helpers/extensions.dart
+++ lib/helpers/extensions.dart
@@ -3,6 +3,8 @@ import 'package:intl/intl.dart';
 
 extension DateFormatter on DateTime {
   String get dMY => DateFormat('dd/MM/yyyy').format(toLocal());
+  String get toHoursMinutes => DateFormat('HH:mm').format(toLocal());
+  String get yMD => DateFormat('yyyy-MM-dd').format(toLocal());
 }
 
 extension TimerFormatter on TimeOfDay {
