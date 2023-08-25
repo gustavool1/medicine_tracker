cedf5d5f4c198691af30306871523224b2ccbf97
gustavool1 <go8895806@gmail.com>
2023-08-25 15:47:52 -0300

    feat: removing unnecessary imports

--- lib/ui/calendar/calendar.dart
+++ lib/ui/calendar/calendar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:intl/intl.dart';
 import 'package:medicine_tracker/styles/styles.dart';
 import 'package:scroll_to_index/scroll_to_index.dart';
