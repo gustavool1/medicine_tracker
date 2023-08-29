be2affb4259237686a9dab121edb42335733f6da
gustavool1 <go8895806@gmail.com>
2023-08-29 12:02:13 -0300

    feat: adding global key to verify form

--- lib/pages/create_pill/create_pill.dart
+++ lib/pages/create_pill/create_pill.dart
@@ -3,8 +3,15 @@ import 'package:medicine_tracker/styles/styles.dart';
 
 import 'widgets/widgets.dart';
 
-class CreatePillPage extends StatelessWidget {
+class CreatePillPage extends StatefulWidget {
   const CreatePillPage({super.key});
+
+  @override
+  State<CreatePillPage> createState() => _CreatePillPageState();
+}
+
+class _CreatePillPageState extends State<CreatePillPage> {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -15,17 +22,20 @@ class CreatePillPage extends StatelessWidget {
       body: Container(
         color: ColorPackage.defaultPrimary,
         padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const RegisterPillName(),
-            const FrequencyPill(),
-            const PillsAmount(),
-            SelectDates(),
-            Reminder(),
-            const Expanded(child: SizedBox()),
-            const SubmitButton()
-          ],
+        child: Form(
+          key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const RegisterPillName(),
+              const FrequencyPill(),
+              const PillsAmount(),
+              SelectDates(),
+              Reminder(),
+              const Expanded(child: SizedBox()),
+              SubmitButton(formKey: formKey)
+            ],
+          ),
         ),
       ),
     );
