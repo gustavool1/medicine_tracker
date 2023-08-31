4fdf6c8908739ac6b2489021f538588da42b8723
gustavool1 <go8895806@gmail.com>
2023-08-31 11:10:05 -0300

    feat: exporting auth on blocs

--- lib/bloc/bloc.dart
+++ lib/bloc/bloc.dart
@@ -1,6 +1,5 @@
 export 'home/home.dart';
 export 'pills/pills.dart';
 export 'calendar/calendar.dart';
-export 'create_pill/create_pill.dart';
-export 'auth/auth.dart';
 export 'medicine/medicine.dart';
+export 'auth/auth.dart';
