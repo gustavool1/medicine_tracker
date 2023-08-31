4895ab34d11a22c468a2b783f832fd32fd639939
gustavool1 <go8895806@gmail.com>
2023-08-31 11:07:11 -0300

    feat: removing create_pill relateds to refactor

--- lib/bloc/create_pill/create_pill.dart
+++
@@ -1,3 +0,0 @@
-export 'create_pill_bloc.dart';
-export 'create_pill_state.dart';
-export 'create_pill_event.dart';
--- lib/bloc/create_pill/create_pill_state.dart
+++
@@ -1,6 +0,0 @@
-import 'package:medicine_tracker/models/models.dart';
-
-class CreatePillState {
-  final CreatePill createPill;
-  CreatePillState(this.createPill);
-}
