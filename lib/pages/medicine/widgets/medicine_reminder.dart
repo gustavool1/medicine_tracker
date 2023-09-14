a1bea706e23375c9a47f692f4ef824cf0c4d771a
gustavool1 <go8895806@gmail.com>
2023-09-14 11:13:54 -0300

    feat: adding a icon on the reminders to a better understanding from the user

--- lib/pages/medicine/widgets/medicine_reminder.dart
+++ lib/pages/medicine/widgets/medicine_reminder.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:medicine_tracker/helpers/extensions.dart';
 import 'package:medicine_tracker/models/models.dart';
 import 'package:medicine_tracker/styles/styles.dart';
 
@@ -9,9 +10,15 @@ class MedicineReminder extends StatelessWidget {
 
   List<Widget> get getPillsReminders {
     return pills.map((pill) {
-      return Text(
-        'pill.timeToTake',
-        style: TextFonts.body1.copyWith(color: ColorPackage.darkBlue),
+      return Row(
+        children: [
+          Text(
+            "As ${pill.timeToTake} do dia ${pill.takePillDay.dM}",
+            style: TextFonts.body1.copyWith(color: ColorPackage.darkBlue),
+          ),
+          if (pill.isTaken) const Icon(Icons.check, color: Colors.green),
+          if (!pill.isTaken) const Icon(Icons.close, color: Colors.red)
+        ],
       );
     }).toList();
   }
