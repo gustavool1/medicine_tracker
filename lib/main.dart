dab2eef28ac37e5deb7795c908fc489a002722e2
gustavool1 <go8895806@gmail.com>
2023-09-14 11:12:15 -0300

    feat: hidding top bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:alarm/alarm.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:medicine_tracker/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+
   await Alarm.init(showDebugLogs: true);
 
   runApp(const App());
